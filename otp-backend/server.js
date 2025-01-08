b489f9f0032b82cbe3e3de8bf1c8a463fc59d1be
Tanish Kothari <tanishkothari@Tanishs-MacBook-Air-2.local>
2025-01-08 14:43:56 +0530

    Second Commit

--- otp-backend/server.js
+++ otp-backend/server.js
@@ -19,5 +19,5 @@ app.get('/otp', (req, res) => {
 });
 const PORT = 3000; 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running!`);
 });
