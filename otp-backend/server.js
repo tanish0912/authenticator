0e2063c055170befb4f1b7189b8375836ac53218
Tanish Kothari <tanishkothari@Tanishs-MacBook-Air-2.local>
2025-01-08 15:43:58 +0530

    Third Commit

--- otp-backend/server.js
+++ otp-backend/server.js
@@ -3,20 +3,27 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors()); 
+
 let currentOTP = null;
+let otpGeneratedAt = null;
+
 const generateOTP = () => {
     return Math.floor(1000 + Math.random() * 9000).toString(); 
 };
+
 const regenerateOTP = () => {
     currentOTP = generateOTP();
-    console.log("New OTP generated:", currentOTP); 
+    otpGeneratedAt = Date.now(); 
+    console.log("New OTP generated:", currentOTP, "at", otpGeneratedAt);
 };
 
 regenerateOTP();
-setInterval(regenerateOTP, 30000);
+setInterval(regenerateOTP, 30000); 
+
 app.get('/otp', (req, res) => {
-    res.json({ otp: currentOTP });
+    res.json({ otp: currentOTP, timestamp: otpGeneratedAt }); 
 });
+
 const PORT = 3000; 
 app.listen(PORT, () => {
     console.log(`Server is running!`);
